5dfc13b171814a88492714034932f839b5dffbb9
Saintxc <zacary.spaulding@gmail.com>
2026-08-10 14:15:43 -0500

    added associate degree to education

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -6,17 +6,29 @@ function Education() {
 
         <h2 className="section-title">Education</h2>
 
-        <article className="education-card">
-          <div>
-            <h3>Full Sail University</h3>
+        <div className="education-list">
+          <article className="education-card">
+            <div>
+              <h3>Full Sail University</h3>
 
-            <p className="degree">Bachelor of Science in Computer Science</p>
+              <p className="degree">Bachelor of Science in Computer Science</p>
 
-            <p>Concentration in Artificial Intelligence</p>
-          </div>
+              <p>Concentration in Artificial Intelligence</p>
+            </div>
 
-          <div className="education-date">Expected September 2026</div>
-        </article>
+            <div className="education-date">Expected September 2026</div>
+          </article>
+
+          <article className="education-card">
+            <div>
+              <h3>Full Sail University</h3>
+
+              <p className="degree">Associate of Science in Computer Science</p>
+            </div>
+
+            <div className="education-date">Graduated August 2025</div>
+          </article>
+        </div>
       </div>
     </section>
   );
